bd44259a49eca6f7d9980aed06c97ee3414cfcb1
bigstar9906 <bigstar9906@naver.com>
2021-05-02 16:42:34 +0900

    Add Block
    블럭 더하기 구현

--- 2048/2048.cs
+++ 2048/2048.cs
@@ -3,6 +3,8 @@ using System;
 
 class Game{
     int[,] Map = new int[4,4];
+    bool isend= false;
+    Random r = new Random();
 public Game()
 {
     this.Map = new int[4,4] {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
@@ -10,6 +12,7 @@ public Game()
 
 public void PrintMap()
 {
+    Console.WriteLine("*****************\n");
     for(int i = 0;i<4;i++)
     {
         for(int j = 0;j<4;j++)
@@ -18,6 +21,45 @@ public void PrintMap()
         }
         Console.WriteLine("\n");
     }
+    Console.WriteLine("*****************\n");
+}
+
+public int SelectVal()
+{
+    int a = r.Next(1,10);
+    if(a>4) return 2;
+    else return 4;
+}
+public void AddBlock()
+{
+    int BlockVal = SelectVal();
+    int PositionNum=0;
+    int temp;
+    int [] Position = new int [16];
+    for(int i = 0;i<4;i++)
+    {
+        for(int j = 0;j<4;j++)
+        {
+            if(this.Map[i,j]==0)
+            {
+                Position[PositionNum] = i*4+j;
+                PositionNum++;
+            }
+        }
+    }
+    if(PositionNum==0)
+    {
+        isend = true;
+        return;
+    }
+    temp = Position[r.Next(0,PositionNum-1)];
+    this.Map[temp/4,temp%4] = BlockVal;
+
+}
+
+public bool End()
+{
+    return this.isend;
 }
 
 }
@@ -28,7 +70,12 @@ class Program
     {
         
         Game g = new Game();
-        g.PrintMap();
+        while(!g.End())
+        {
+            g.PrintMap();
+            g.AddBlock();
+        }
+        Console.WriteLine("\n\nThis is end.");
 
 
         //종료 방지를 위해 작성
